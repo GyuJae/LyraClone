4be673bf56a57f7c6773a46998a26ef4fdcd7d4d
GyuJae <wjdrbwo1206@naver.com>
2025-01-30 17:17:43 +0900

    AssetManager

---
+++ Source/LyraClone/System/GyuAssetManager.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GyuAssetManager.h"
+#include "LyraClone/GyuLogChannels.h"
+
+UGyuAssetManager::UGyuAssetManager()
+{
+}
+
+UGyuAssetManager& UGyuAssetManager::Get()
+{
+	check(GEngine);
+
+	if (UGyuAssetManager* Singleton = Cast<UGyuAssetManager>(GEngine->AssetManager))
+	{
+		return *Singleton;
+	}
+	
+	UE_LOG(LogGyu, Fatal, TEXT("Invalid AssetManager in GameInstance"));
+	return *NewObject<UGyuAssetManager>();
+}
+
+bool UGyuAssetManager::ShouldLogAssetLoads()
+{
+	const TCHAR* CommandLineContent = FCommandLine::Get();
+	static bool bLogAssetLoads = FParse::Param(CommandLineContent, TEXT("LogAssetLoads"));
+	return bLogAssetLoads;
+}
+
+UObject* UGyuAssetManager::SynchronousLoadAsset(const FSoftObjectPath& AssetPath)
+{
+	if (AssetPath.IsValid())
+	{
+		TUniquePtr<FScopeLogTime> LogTimePtr;
+
+		if (ShouldLogAssetLoads())
+		{
+			LogTimePtr = MakeUnique<FScopeLogTime>(*FString::Printf(TEXT("Synchronously loaded asset [%s]"), *AssetPath.ToString()), nullptr, FScopeLogTime::ScopeLog_Seconds);
+		}
+
+		if (UAssetManager::IsInitialized())
+		{
+			return UAssetManager::GetStreamableManager().LoadSynchronous(AssetPath, false);
+		}
+
+		// Use LoadObject if asset manager isn't ready yet.
+		return AssetPath.TryLoad();
+	}
+
+	return nullptr;
+}
+
+void UGyuAssetManager::AddLoadedAsset(const UObject* Asset)
+{
+	if (ensureAlways(Asset))
+	{
+		FScopeLock LoadedAssetsLock(&LoadedAssetsCritical);
+		LoadedAssets.Add(Asset);
+	}
+}
---
+++ Source/LyraClone/System/GyuAssetManager.h
@@ -0,0 +1,96 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "GyuAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LYRACLONE_API UGyuAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+public:
+	UGyuAssetManager();
+
+	static UGyuAssetManager& Get();
+	
+	static bool ShouldLogAssetLoads();
+	static UObject* SynchronousLoadAsset(const FSoftObjectPath& AssetPath);
+
+	// Returns the asset referenced by a TSoftObjectPtr.  This will synchronously load the asset if it's not already loaded.
+	template<typename AssetType>
+	static AssetType* GetAsset(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepInMemory = true);
+
+	// Returns the subclass referenced by a TSoftClassPtr.  This will synchronously load the asset if it's not already loaded.
+	template<typename AssetType>
+	static TSubclassOf<AssetType> GetSubclass(const TSoftClassPtr<AssetType>& AssetPointer, bool bKeepInMemory = true);
+
+protected:
+	// Thread safe way of adding a loaded asset to keep in memory.
+	void AddLoadedAsset(const UObject* Asset);
+
+private:
+
+	// Assets loaded and tracked by the asset manager. GC 대상
+	UPROPERTY()
+	TSet<TObjectPtr<const UObject>> LoadedAssets;
+
+	// Used for a scope lock when modifying the list of load assets. Object 단위 Locking
+	FCriticalSection LoadedAssetsCritical;
+};
+
+template<typename AssetType>
+AssetType* UGyuAssetManager::GetAsset(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepInMemory)
+{
+	AssetType* LoadedAsset = nullptr;
+
+	const FSoftObjectPath& AssetPath = AssetPointer.ToSoftObjectPath();
+
+	if (AssetPath.IsValid())
+	{
+		LoadedAsset = AssetPointer.Get();
+		if (!LoadedAsset)
+		{
+			LoadedAsset = Cast<AssetType>(SynchronousLoadAsset(AssetPath));
+			ensureAlwaysMsgf(LoadedAsset, TEXT("Failed to load asset [%s]"), *AssetPointer.ToString());
+		}
+
+		if (LoadedAsset && bKeepInMemory)
+		{
+			// Added to loaded asset list.
+			Get().AddLoadedAsset(Cast<UObject>(LoadedAsset));
+		}
+	}
+
+	return LoadedAsset;
+}
+
+template<typename AssetType>
+TSubclassOf<AssetType> UGyuAssetManager::GetSubclass(const TSoftClassPtr<AssetType>& AssetPointer, bool bKeepInMemory)
+{
+	TSubclassOf<AssetType> LoadedSubclass;
+
+	const FSoftObjectPath& AssetPath = AssetPointer.ToSoftObjectPath();
+
+	if (AssetPath.IsValid())
+	{
+		LoadedSubclass = AssetPointer.Get();
+		if (!LoadedSubclass)
+		{
+			LoadedSubclass = Cast<UClass>(SynchronousLoadAsset(AssetPath));
+			ensureAlwaysMsgf(LoadedSubclass, TEXT("Failed to load asset class [%s]"), *AssetPointer.ToString());
+		}
+
+		if (LoadedSubclass && bKeepInMemory)
+		{
+			// Added to loaded asset list.
+			Get().AddLoadedAsset(Cast<UObject>(LoadedSubclass));
+		}
+	}
+
+	return LoadedSubclass;
+}
